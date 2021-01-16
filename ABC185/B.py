b607500ccc67a0728dfb2afdcdc6a0353cb20c90
funayama <hiro19323@gmail.com>
2021-01-16 23:11:46 +0900

    2021 1/16 commit

---
+++ ABC185/B.py
@@ -0,0 +1,16 @@
+N, M, T = map(int, input().split())
+A = []
+B = []
+for i in range(M):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
+
+for i in range(T):
+    if i in A:
+        
+
+if ans > 0:
+    print('Yes')
+else:
+    print('No')
